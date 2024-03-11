2a339df2f10d239fb7ea80840153196ce6cb9a1a
Kwansing_Ng <kwansing_ng@singaporeair.com.sg>
2024-03-11 10:11:51 +0800

    batman

---
+++ lua/plugins/poimandres.lua
@@ -0,0 +1,11 @@
+return { 
+  'olivercederborg/poimandres.nvim',
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "poimandres",
+    },
+  },
+}
