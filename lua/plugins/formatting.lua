2e6933b8fc33ba5d97f203f048d4280f9ad1f7b2
kwansing14 <kwansing@hotmail.sg>
2024-03-22 08:07:49 +0800

    feat: svelte syntax

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -32,6 +32,20 @@ return {
         ["handlebars"] = { "prettier" },
       },
     },
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      event = "VeryLazy",
+    },
+    {
+      "echasnovski/mini.comment",
+      event = "VeryLazy",
+      opts = {
+        options = {
+          custom_commentstring = function()
+            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
+      },
+    },
   },
 }
-
--- lua/plugins/syntaxHighlight.lua
+++ lua/plugins/syntaxHighlight.lua
@@ -1,4 +1,18 @@
 return {
-  'wuelnerdotexe/vim-astro',
-  event = "VeryLazy",
+  {
+    "wuelnerdotexe/vim-astro",
+    event = "VeryLazy",
+  },
+  {
+    "evanleck/vim-svelte",
+    event = "VeryLazy",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "svelte",
+      },
+    },
+  },
 }
