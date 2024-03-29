d306d5f9832def706c3febf4e11e68b9c98d6f59
kwansing14 <kwansing@hotmail.sg>
2024-03-30 00:02:13 +0800

    diffview added imply local

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,8 +2,8 @@ return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   opts = {
-    default_args = {    -- Default args prepended to the arg-list for the listed commands
+    default_args = { -- Default args prepended to the arg-list for the listed commands
       DiffviewOpen = { "--imply-local" },
     },
-  } 
+  },
 }
