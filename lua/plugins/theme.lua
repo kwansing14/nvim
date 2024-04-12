9dbffd6e762b262bd8667903caa47d0fce8d62cf
Kwansing_Ng <kwansing_ng@singaporeair.com.sg>
2024-04-12 17:08:30 +0800

    feat: new them lets go

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "oahlen/iceberg.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "miikanissi/modus-themes.nvim",
+    event = "VeryLazy", -- Configure LazyVim to load gruvbox
+  },
+  {
+    "catppuccin/nvim",
+    event = "VeryLazy", -- Configure LazyVim to load gruvbox
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "modus" },
+  },
+}
