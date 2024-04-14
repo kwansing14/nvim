f5cd6c177f9af26781d3628ee84151ddaef9634f
kwansing14 <kwansing@hotmail.sg>
2024-04-14 18:21:16 +0800

    fixed diffview with theme using autocmd to reset color

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.cmd("hi DiffAdd    guibg=#283B4D guifg=NONE")
+    vim.cmd("hi DiffChange guibg=#283B4D guifg=NONE")
+    vim.cmd("hi DiffDelete guibg=#3C2C3C guifg=#4d384d")
+    vim.cmd("hi DiffText   guibg=#365069 guifg=NONE")
+  end,
+})
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -6,4 +6,8 @@ return {
       DiffviewOpen = { "--imply-local" },
     },
   },
+  -- hi DiffAdd    guibg=#283B4D guifg=NONE
+  --       hi DiffChange guibg=#283B4D guifg=NONE
+  --       hi DiffDelete guibg=#3C2C3C guifg=#4d384d
+  --       hi DiffText   guibg=#365069 guifg=NONE
 }
