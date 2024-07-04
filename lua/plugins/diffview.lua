4095f587eaa88478b46336650f965c2d439c1db5
Kwansing_Ng <kwansing_ng@singaporeair.com.sg>
2024-07-04 14:30:14 +0800

    update

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,11 +5,11 @@ return {
     default_args = { -- Default args prepended to the arg-list for the listed commands
       -- DiffviewOpen = { "--imply-local" },
     },
-    hooks = {
-      diff_buf_win_enter = function()
-        vim.opt_local.foldenable = false
-      end,
-    },
+    -- hooks = {
+    --   diff_buf_win_enter = function()
+    --     vim.opt_local.foldenable = false
+    --   end,
+    -- },
   },
   -- hi DiffAdd    guibg=#283B4D guifg=NONE
   --       hi DiffChange guibg=#283B4D guifg=NONE
